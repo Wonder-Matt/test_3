ba56823195065eda1c7f64f68d93e694df36e928
Wonder-Matt <159161107+Wonder-Matt@users.noreply.github.com>
2024-02-07 12:25:08 +0100

    Update signup.js

--- signup.js
+++ signup.js
@@ -4,7 +4,7 @@ document.getElementById("formulario-crear").addEventListener("submit", (event) =
   event.preventDefault();
 
   const email = document.getElementById("email").value;
-  const username = document.getElementById("username").value;
+  
   const password = document.getElementById("password").value;
 
   const account = new ManageAccount();
